3a30de144041251d278afdf29106fd30b699ca94
DeveloperMa7moud <dev.mahmoud.khalaf@gmail.com>
2026-07-06 01:32:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let birthyear = document.getElementById("birthyearInput");
+let button = document.getElementById("calculatorbtn");
+let result = document.getElementById("result");
+button.addEventListener("click",function(){
+    birthyear = birthyear.value;
+    if(birthyear === ""){
+        result.textContent = "Please enter your birth year to know your age."
+        return;
+    }
+    let age = 2026 - birthyear;
+    result.textContent = "Your age is " + age ;
+})
